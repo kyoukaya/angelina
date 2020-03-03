5292db242e760349e0e3b5067dced5a12567dd01
kyoukaya <kyoukaya@gmail.com>
2020-03-03 22:23:03 +0800

    add no-unk-json flag

--- cmd/main.go
+++ cmd/main.go
@@ -19,6 +19,7 @@ var unsafeOrigin = flag.Bool("unsafe-origin", false, "allow any HTTP request, "+
 	"no matter what origin they specify, to upgrade into a ws connection")
 var staticDir = flag.String("ange-static", "", "path to static files to serve on the root URL. Serving disabled if empty string.")
 var angeHost = flag.String("ange-host", ":8000", "host on which ange is served")
+var noUnknownJSON = flag.Bool("no-unk-json", false, "disallows unknown fields when unmarshalling json in the gamestate module")
 
 func main() {
 	flag.Parse()
@@ -31,6 +32,7 @@ func main() {
 		VerboseGoProxy:   *verboseGoProxy,
 		Address:          *host,
 		DisableCertStore: *disableCertStore,
+		NoUnknownJSON:    *noUnknownJSON,
 	}
 	rhine := proxy.NewProxy(options)
 	ange := angelina.New(*staticDir, *angeHost, *unsafeOrigin)
--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.13
 require (
 	github.com/elazarl/goproxy v0.0.0-20190711103511-473e67f1d7d2
 	github.com/gorilla/websocket v1.4.1
-	github.com/kyoukaya/rhine v0.0.0-20200229060759-716474a467a5
+	github.com/kyoukaya/rhine v0.0.0-20200302044913-7347ffb5d8c7
 )
