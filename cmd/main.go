f62893f92304a86e89d3b487d7e22cf973c765d9
kyoukaya <kyoukaya@gmail.com>
2020-03-13 22:33:13 +0800

    default unsafe-origin flag to true If users expose angelina ports to the WAN, they should do so at their own risk. Any person determined to access an angelina server wouldn't be sending an origin header or abiding by CORs.

--- cmd/main.go
+++ cmd/main.go
@@ -15,7 +15,7 @@ var verbose = flag.Bool("v", false, "print Rhine verbose messages")
 var verboseGoProxy = flag.Bool("v-goproxy", false, "print verbose goproxy messages")
 var host = flag.String("host", ":8080", "host on which the proxy is served")
 var disableCertStore = flag.Bool("disable-cert-store", false, "disables the built in certstore, reduces memory usage but increases HTTP latency and CPU usage")
-var unsafeOrigin = flag.Bool("unsafe-origin", false, "allow any HTTP request, "+
+var unsafeOrigin = flag.Bool("unsafe-origin", true, "allow any HTTP request, "+
 	"no matter what origin they specify, to upgrade into a ws connection")
 var staticDir = flag.String("ange-static", "", "path to static files to serve on the root URL. Serving disabled if empty string.")
 var angeHost = flag.String("ange-host", ":8000", "host on which ange is served")
